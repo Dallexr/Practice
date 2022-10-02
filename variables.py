1d0359029342d2868f87ac1b95e0e9e790241351
Dallexr <73978644+Dallexr@users.noreply.github.com>
2022-10-02 02:27:35 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Declaring a variable 
+userAge = 0
+
+# Declaring multiple variables
+ageOfUser, userName = 30, 'Peter'
+
+# Variable names in Python can only contain letters (a-z, A-Z), numbers or underscores (_). However the first 
+# character cannot be a number. There are certain reserved words that cannot be used as a variable, such as:
+# print, input, if, while, etc..
+
+# Variable names are case sensitive
+
+x = 5
+y = 10
+y = x
+print ("x = ", x)
+print ("y = ", y)
+
